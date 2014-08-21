a85506c183ebd3ddd723093102b7462eedf9edd5
lennart.aasenden@gmail.com <lennart.aasenden@gmail.com@fe4eb8a3-9c1e-8602-05e7-6ddae0620671>
2014-08-21 16:43:24 +0000

    -Added: TQTXLabel added to codebase. A simple PRE based label with no horizontal alignment. -Altered: Added BeforeEffect and AfterEffect handlers to qtxEffects

---
+++ trunk/qtxlabel.pas
@@ -0,0 +1,114 @@
+unit qtxlabel;
+
+//#############################################################################
+//
+//  Unit:       qtxlabel.pas
+//  Author:     Jon Lennart Aasenden [cipher diaz of quartex]
+//  Copyright:  Jon Lennart Aasenden, all rights reserved
+//
+//  Description:
+//  ============
+//  This unit contains a more lightweight label control.
+//  It has no horizontal alignment, but for pure "text" labels which doesnt
+//  need special treatment, this control is much better than TW3Label.
+//
+//  _______           _______  _______ _________ _______
+// (  ___  )|\     /|(  ___  )(  ____ )\__   __/(  ____ \|\     /|
+// | (   ) || )   ( || (   ) || (    )|   ) (   | (    \/( \   / )
+// | |   | || |   | || (___) || (____)|   | |   | (__     \ (_) /
+// | |   | || |   | ||  ___  ||     __)   | |   |  __)     ) _ (
+// | | /\| || |   | || (   ) || (\ (      | |   | (       / ( ) \
+// | (_\ \ || (___) || )   ( || ) \ \__   | |   | (____/\( /   \ )
+// (____\/_)(_______)|/     \||/   \__/   )_(   (_______/|/     \|
+//
+//
+//#############################################################################
+
+interface
+
+uses 
+  W3System, w3graphics, w3components,
+  qtxutils;
+
+type
+
+  TQTXLabel = Class(TW3CustomControl)
+  private
+    FAuto:    Boolean;
+  protected
+    procedure AdjustSize;virtual;
+    procedure setAuto(const aValue:Boolean);
+    function  getCaption:String;
+    Procedure setCaption(Const aValue:String);
+    function  MakeElementTagId: String; override;
+    procedure InitializeObject;override;
+  published
+    Property  Autosize:Boolean read FAuto write setAuto;
+    Property  Caption:String read getCaption write setCaption;
+  End;
+
+implementation
+
+//############################################################################
+// TQTXLabel
+//############################################################################
+
+procedure TQTXLabel.InitializeObject;
+Begin
+  inherited;
+  setAuto(True);
+  setCaption(ClassName);
+  TQTXTools.ExecuteOnElementReady(Handle, Procedure ()
+    Begin
+      //Handle.style['overflow']:='hidden';
+      AdjustSize;
+    end);
+end;
+
+procedure TQTXLabel.AdjustSize;
+var
+  mSize: TQTXTextMetric;
+Begin
+  if FAuto then
+  Begin
+    mSize:=TQTXTools.calcTextMetrics(innerHTML,Font.name,font.size);
+    if (mSize.tmWidth<1)
+    or (mSize.tmHeight<1) then
+    Begin
+      (* no size is only valid if caption is empty *)
+      if getCaption.Length>0 then
+      exit else
+      self.SetSize(mSize.tmWidth,mSize.tmHeight);
+    end else
+    self.SetSize(mSize.tmWidth,mSize.tmHeight);
+  end;
+end;
+
+procedure TQTXLabel.setAuto(const aValue:Boolean);
+Begin
+  if aValue<>FAuto then
+  Begin
+    FAuto:=aValue;
+    if FAuto then
+    AdjustSize;
+  end;
+end;
+
+function TQTXLabel.getCaption:String;
+Begin
+  result:=innerHTML;
+end;
+
+Procedure TQTXLabel.setCaption(Const aValue:String);
+Begin
+  innerHTML:=aValue;
+  AdjustSize;
+end;
+
+function  TQTXLabel.MakeElementTagId: String;
+Begin
+  result:='PRE';
+end;
+
+
+end.
